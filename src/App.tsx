681a380dd83a06483d4258e43058be8a1179224c
ennskevin <ennskevin@tutanota.com>
2026-02-22 12:13:48 -0700

    Add deploy.yml for AWS deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { nanoid } from "nanoid"
 import { clsx } from "clsx"
 import Confetti from "react-confetti"
